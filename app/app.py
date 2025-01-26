dc5bc91f83bf90b0b26c14da7783ecb1452e6d45
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-01-26 23:52:20 +0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,45 @@
+# /app/app.py
+from flask import Flask, render_template, request, redirect, url_for
+from .init import create_app, db
+from .models import Product
+
+app = create_app()
+
+@app.route('/')
+def index():
+    products = Product.query.all()
+    return render_template('index.html', products=products)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_product():
+    if request.method == 'POST':
+        name = request.form['name']
+        quantity = int(request.form['quantity'])
+        price = float(request.form['price'])
+        new_product = Product(name=name, quantity=quantity, price=price)
+        db.session.add(new_product)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add_product.html')
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update_product(id):
+    product = Product.query.get(id)
+    if request.method == 'POST':
+        product.name = request.form['name']
+        product.quantity = int(request.form['quantity'])
+        product.price = float(request.form['price'])
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update_product.html', product=product)
+
+@app.route('/delete/<int:id>')
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
