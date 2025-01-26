0c5ce3d6c6777c34cb4d7eace5b034631d65ff67
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-01-26 23:52:56 +0600

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,9 @@
+# /app/models.py
+from .init import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
 
